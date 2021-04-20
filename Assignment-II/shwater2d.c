b15b9a1e0c9ce271c9179d945b08bfeab4155e62
steven <wdchien@kth.se>
2021-04-20 16:34:17 +0200

    add missing save_vtk function

--- Assignment-II/shwater2d.c
+++ Assignment-II/shwater2d.c
@@ -132,7 +132,40 @@ void solver(double *Q, double **ffx, double **ffy, double **nFx, double **nFy,
   
   }
 }
-  
+
+void save_vtk(double *Q, double *x, double *y, int m, int n) {
+
+  int i, j;
+  FILE *fp = fopen("result.vtk", "w");
+
+  /* Write vtk Datafile header */
+  fprintf(fp, "# vtk DataFile Version 2.0\n");
+  fprintf(fp, "VTK\nASCII\nDATASET POLYDATA\n");
+
+  /* Store water height as polydata */
+  fprintf(fp, "\nPOINTS %d double\n", m*n);
+
+  for (j = 0; j < n; j++)
+    for (i = 0; i < m; i++)
+      fprintf(fp, "%e %e %e\n", x[i], y[j], Q(0, i, j));
+
+  fprintf(fp,"\nVERTICES %d %d\n", n, n *(m+1));
+  for (j = 0; j < n; j++)  {
+    fprintf(fp, "%d ", m);
+    for (i = 0; i < m; i++)
+      fprintf(fp, "%d ", i+j*m);
+    fprintf(fp,"\n");
+  }
+
+  /* Store lookup table */
+  fprintf(fp,
+	  "POINT_DATA %d\nSCALARS height double 1\nLOOKUP_TABLE default\n",m*n);
+  for (j = 0; j < n; j++)
+    for (i = 0; i < m; i++)
+      fprintf(fp, "%e\n", Q(0, i, j));
+  fclose(fp);
+}
+
 /*
   This is the main routine of the program, which allocates memory 
   and setup all parameters for the problem.
@@ -249,6 +282,3 @@ int main(int argc, char **argv) {
 
   return 0;    
 }
-
-
-
