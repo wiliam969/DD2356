1d45c0b27d89c43df9ad8af0d6ca026859edc909
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-04-18 15:44:52 +0200

    ex1 hwmp

---
+++ Assignment-III/hwmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char argv[]){
+
+/*
+ml PDC/23.12
+ml cpeGNU/23.12
+gcc -fopenmp hwmp.c -o hwmp.x^C
+export OMP_NUM_THREADS=4
+salloc -n 1 -t 0:10:00 -A edu25.DD2356 -p shared
+srun -n 1 ./hwmp.x
+
+*/
+
+
+    omp_set_num_threads(4); // Set the number of threads to 4
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num(); // Get the thread ID
+        printf("Hello World from thread %d!\n", thread_id); // Print the thread ID
+    }
+    return 0;
+}
