98cc5eb3c3e52dbe15e72a7e5d2bdf2314c7eef5
Robin Witte <wiliam969@icloud.com>
2025-04-24 14:38:48 +0200

    adding hwloc

---
+++ Assignment-II/batch_scripts/ex_1_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+#SBATCH -A edu25.DD2356
+#SBATCH -J hw_loc
+#SBATCH -p shared
+#SBATCH -t 00:10:00
+#SBATCH --nodes=1
+#SBATCH --mem=128G
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+
+# Run the executable named main.x and write the output to my_output_file.txt
+srun hwloc-ls --of png > topology.png
