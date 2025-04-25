9e69979a63f515636a9280a58d2104260194aa3d
Robin Witte <rwitte@login1.hsn.dardel.pdc.kth.se>
2025-04-25 23:50:08 +0200

    topology

--- Assignment-II/batch_scripts/ex_1_batch.sh
+++ Assignment-II/batch_scripts/ex_1_batch.sh
@@ -4,11 +4,10 @@
 #SBATCH -A edu25.DD2356
 #SBATCH -J hw_loc
 #SBATCH -p shared
-#SBATCH -t 00:10:00
+#SBATCH -t 00:1:00
 #SBATCH --nodes=1
-#SBATCH --mem=128G
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=128
 
 # Run the executable named main.x and write the output to my_output_file.txt
-srun hwloc-ls --output-format graphical topology.svg
+srun hwloc-ls --of svg > topology.svg
