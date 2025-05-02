e580ff0745455a1b5b1633e678658df25f077efe
Robin Witte <wiliam969@icloud.com>
2025-05-02 12:46:11 +0200

    serial version wtf

--- Assignment-III/serial_sum.c
+++ Assignment-III/serial_sum.c
@@ -11,11 +11,35 @@ void generate_random(double *input, size_t size)
 
 double serial_sum(double *x, size_t size)
 {
+  double start_time, end_time;
+
+  start_time = omp_get_wtime();
   double sum_val = 0.0;
 
   for (size_t i = 0; i < size; i++) {
     sum_val += x[i];
   }
 
+  end_time = omp_get_wtime();
+
+  printf("Execution time with scheduling: %f seconds\n", end_time - start_time);
+
   return sum_val;
+}
+
+int main(int argc, char argv[]){
+
+  if(argc < 2){
+    printf("!!\tERROR: Expected 1 argument, got %d\n", argc - 1);
+    MPI_Finalize();
+    return 1;
+  }
+
+  int N = atoi(argv[1]);
+
+  double *n_input = (double *)malloc(N * sizeof(double));
+
+  generate_random(n_input, N); 
+
+  serial_sum(n_input, N); 
 }
