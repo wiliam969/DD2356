cb61991e3ecfda80b4082b4a6e0493a9d1a29d40
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-05-12 12:48:01 +0200

    perf to serial

--- Assignment-IV/batch_scripts/ex_1_serial.sh
+++ Assignment-IV/batch_scripts/ex_1_serial.sh
@@ -10,4 +10,4 @@
 #SBATCH -e ex1_serial.stderr
 
  cc -lm -O3 -march=native -DN=10000 -DSTEPS=500 -DIO_ON_OFF=0 ../ex_1/ex_1_serial.c -o ../bin/ex_1_serial.out
-    srun -n 1 ../bin/ex_1_mpi_mr.out > "../batch_output/ex1_serial.stdout"    
+    srun perf stat --to-json -e L1-dcache-load-misses,power/energy-cores/,L1-dcache-loads,L1-dcache-prefetches,L1-icache-load-misses,L1-icache-loads,duration_time,cpu-cycles,instructions -n 1 ../bin/ex_1_serial.out > "../batch_output/ex_1_serial.stdout"    
