504709cd5c9c0249d82c66740581418b448372e4
Robin Witte <wiliam969@icloud.com>
2025-05-15 18:00:25 +0200

    wrong pipelining strategy used

--- Assignment-IV/batch_scripts/ex_1_mpi_N4.sh
+++ Assignment-IV/batch_scripts/ex_1_mpi_N4.sh
@@ -13,5 +13,5 @@ process_count=(1 2 4 8)
 
 for proc in "${process_count[@]}"; do
     cc -lm -O3 -march=native -DN=10000 -DSTEPS=500 -DIO_ON_OFF=0 ../ex_1/ex_1_mpi.c -o ../bin/ex_1_mpi_N4.out
-    srun -n ${proc} perf stat -e L1-dcache-load-misses,power/energy-cores/,L1-dcache-loads,L1-dcache-prefetches,L1-icache-load-misses,L1-icache-loads,duration_time,cpu-cycles,instructions ../bin/ex_1_mpi_N4_${proc}.out > ex_1_mpi_N4_${proc}.txt ../bin/ex_1_mpi_N4_${proc}.out > ../batch_output/ex1_mpi_N4_${proc}.stdout    
+    srun -n ${proc} perf stat -e L1-dcache-load-misses,power/energy-cores/,L1-dcache-loads,L1-dcache-prefetches,L1-icache-load-misses,L1-icache-loads,duration_time,cpu-cycles,instructions ../bin/ex_1_mpi_N4.out > ../batch_output/ex1_mpi_N4_${proc}.stdout    
 done
