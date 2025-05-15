9fef8ac6f07acc91498a5449d6899559fdf6657a
Robin Witte <wiliam969@icloud.com>
2025-05-15 13:30:25 +0200

    ex4 serial stuff

--- Assignment-IV/batch_scripts/ex_4_serial.sh
+++ Assignment-IV/batch_scripts/ex_4_serial.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH -J A4-ex4-n1
+#SBATCH -J A4-ex4-ser
 #SBATCH -t 0:20:00
 #SBATCH -A edu25.dd2356
 #SBATCH -p shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH -e A4-ex4-n1.stderr
+#SBATCH -e A4-ex4-ser.stderr
 matrix_sizes=(1000 10000 100000 1000000)
 
 . ./perf_env
@@ -14,7 +14,7 @@ matrix_sizes=(1000 10000 100000 1000000)
 # Run and redirect output
 for proc in "${process_count[@]}"; do
     for size in "${matrix_sizes[@]}"; do
-        cc -lm -O3 -march=native -DN=${size} ../ex4/blas_mpi.c -o ../bin/blas_mpi.out
-        srun -n ${proc} perf stat -e ${events} -o mpi_game-stat.stat ../bin/blas_mpi.out > ../batch_output/ex4_mpi_${proc}_${size}_output.stdout
+        cc -lm -O3 -march=native -DN=${size} ../ex4/blas_serial.c -o ../bin/blas_serial.out
+        srun -n ${proc} perf stat -e ${events} -o mpi_game-stat.stat ../bin/blas_serial.out > ../batch_output/ex4_serial_${proc}_${size}_output.stdout
     done
 done
