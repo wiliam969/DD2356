bdbca080798b8ca66e535abf59c14fe47ccefeb5
Robin Witte <wiliam969@icloud.com>
2025-05-16 17:45:26 +0200

    compile issues with predefined statements because blas is using the same makro lets go

--- Assignment-IV/ex4/blas_serial.c
+++ Assignment-IV/ex4/blas_serial.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <cblas.h>
 
-#ifndef N
-#define N 1000  // Matrix size
+#ifndef MATRIX_SIZE
+#define MATRIX_SIZE 1000  // Matrix size
 #endif
 
 void initialize(double *matrix, double *vector) {
-    for (int i = 0; i < N * N; i++) {
+    for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; i++) {
         matrix[i] = (double)(i % 100) / 10.0;
     }
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
         vector[i] = (double)(i % 50) / 5.0;
     }
 }
 
 int main() {
-    double *matrix = (double*) malloc(N * N * sizeof(double));
-    double *vector = (double*) malloc(N * sizeof(double));
-    double *result = (double*) malloc(N * sizeof(double));
+    double *matrix = (double*) malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
+    double *vector = (double*) malloc(MATRIX_SIZE * sizeof(double));
+    double *result = (double*) malloc(MATRIX_SIZE * sizeof(double));
     
     initialize(matrix, vector);
     
     // Perform matrix-vector multiplication using BLAS
-    cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, 1.0, matrix, N, vector, 1, 0.0, result, 1);
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, MATRIX_SIZE, MATRIX_SIZE, 1.0, matrix, MATRIX_SIZEN, vector, 1, 0.0, result, 1);
     
     // Write output to file
     FILE *f = fopen("blas_serial_output.txt", "w");
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
         fprintf(f, "%f\n", result[i]);
     }
     fclose(f);
