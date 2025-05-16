6632139f7b905aae828c7a9e44294d1feb1b2764
Robin Witte <wiliam969@icloud.com>
2025-05-16 07:49:54 +0200

    power energy cores not supported by dardel

--- Assignment-IV/batch_scripts/ex_1_mpi_N1.sh
+++ Assignment-IV/batch_scripts/ex_1_mpi_N1.sh
@@ -13,5 +13,5 @@ process_count=(1 2 4 8)
 
 for proc in "${process_count[@]}"; do
     cc -lm -O3 -march=native -DN=10000 -DSTEPS=500 -DIO_ON_OFF=0 ../ex_1/ex_1_mpi.c -o ../bin/ex_1_mpi_N1.out
-    srun -n ${proc} perf stat -e L1-dcache-load-misses,power/energy-cores/,L1-dcache-loads,L1-dcache-prefetches,L1-icache-load-misses,L1-icache-loads,duration_time,cpu-cycles,instructions ../bin/ex_1_mpi_N1.out > ../batch_output/ex1_mpi_N1_${proc}.stdout    
+    srun -n ${proc} perf stat -e L1-dcache-load-misses,power/energy-pkg/,L1-dcache-loads,L1-dcache-prefetches,L1-icache-load-misses,L1-icache-loads,duration_time,cpu-cycles,instructions ../bin/ex_1_mpi_N1.out > ../batch_output/ex1_mpi_N1_${proc}.stdout    
 done
