f9eae5e75329871f62bc45f4159dd5c255d45a35
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-05-17 10:21:45 +0200

    scorep test

--- Assignment-IV/batch_scripts/ex_3_parallel_sp_2_t.sh
+++ Assignment-IV/batch_scripts/ex_3_parallel_sp_2_t.sh
@@ -5,7 +5,7 @@
 #SBATCH -A edu25.dd2356
 #SBATCH -p shared
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH -e ex3_mpi2t.stderr
 
@@ -14,7 +14,7 @@ module load PDC/23.12
 module load score-p/8.4-cpeGNU
 export SCOREP_ENABLE_PROFILING=false
 export SCOREP_ENABLE_TRACING=true
-scorep cc -lm -O3 -march=native ../ex2/ex_2_parallel_sum.c -o ../bin/ex3_sp_ex2.out
+scorep mpicc -lm -O3 -march=native ../ex2/ex_2_parallel_sum.c -o ../bin/ex3_sp_ex2.out
 
 # Run and redirect output
 srun -n 8 ../bin/ex3_sp_ex2.out > "../batch_output/ex3_sp_ex2.stdout"
