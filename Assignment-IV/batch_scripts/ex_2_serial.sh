cd0fbe59948e9a48136af58172e0bb9dfdc69cb3
Robin Witte <wiliam969@icloud.com>
2025-05-17 11:05:04 +0200

    serial version missing

--- Assignment-IV/batch_scripts/ex_2_serial.sh
+++ Assignment-IV/batch_scripts/ex_2_serial.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -l
 
 #SBATCH -J A4-ex2-serial
-#SBATCH -t 0:05:00
+#SBATCH -t 1:45:00
 #SBATCH -A edu25.dd2356
 #SBATCH -p shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH -e ex2_serial_sum.stderr
+#SBATCH --mem=16G
+matrix_sizes=(1000 10000 100000 1000000)
 
-gcc -O3 -fopenmp ../ex2/ex2_serial_sum.c -o ../bin/ex2_serial_sum.out
-
-# Run and redirect output
-srun -n 1 ../bin/ex2_serial_sum.out > ../batch_output/ex_2_serial_output.stdout
+for size in "${matrix_sizes[@]}"; do
+    gcc -O3 -fopenmp -DN=${size} ../ex2/ex2_serial_sum.c -o ../bin/ex2_serial_sum.out
+    # Run and redirect output
+    srun -n 1 perf stat -e ${events} -o ex2_serial_${size}.data ../bin/ex2_serial_sum.out >> ../batch_output/ex_2_serial_output.stdout
+done
